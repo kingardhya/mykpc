b3f8fd20f590e8c4b10db99e585bf0beff2e0155
Putra <Ardhya.Putra@kpc.co.id>
2021-05-20 09:00:42 +0800

    kantor

---
+++ src/pages/Eleave/EleaveNew/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const EleaveNew = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default EleaveNew
+
+const styles = StyleSheet.create({})
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,27 @@ import Home from './Home'
 import Akun from './Akun'
 import Splash from './Splash'
 import Eform from './Eform'
+import EformNew from './Eform/EformNew'
+import EformListApproval from './Eform/EformListApproval'
+import EformHistory from './Eform/EformHistory'
 import Video from './Video'
 import Login from './Login'
+import Eabsensi from './Eabsensi'
+import EabsensiCheck from './Eabsensi/EabsensiCheck'
+import EabsensiCorrection from './Eabsensi/EabsensiCorrection'
+import EabsensiHistory from './Eabsensi/EabsensiHistory'
+import Eleave from './Eleave'
+import EleaveApproval from './Eleave/EleaveApproval'
+import EleaveHistory from './Eleave/EleaveHistory'
+import EleaveListApproval from './Eleave/EleaveListApproval'
+import EleaveNew from './Eleave/EleaveNew'
+import Epay from './Epay'
+import EpayHistory from './Epay/EpayHistory'
+import EpayThisMonth from './Epay/EpayThisMonth'
+import PerformanceEmployee from './PerformanceEmployee'
+import PerformanceEmployeeHistory from './PerformanceEmployee/PerformanceEmployeeHistory'
 
 
-export {Home, Akun, Splash, Eform, Video, Login}
+export {Home, Akun, Splash, Eform, Video, Login, EformNew, EformHistory, EformListApproval,Eabsensi, EabsensiCheck,
+        EabsensiCorrection, EabsensiHistory, Eleave, EleaveApproval, EleaveHistory, EleaveListApproval, EleaveNew,
+        Epay, EpayHistory, EpayThisMonth, PerformanceEmployee, PerformanceEmployeeHistory}
